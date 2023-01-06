0c0e8c125e94207592b64dc8864ee1f85e930356
Michael Chan <michaelcychan@gmail.com>
2023-01-06 15:15:14 +0000

    implemented merge sort with Go

---
+++ bubbleSort-Go/mergeSort.go
@@ -0,0 +1,32 @@
+package MySort
+
+func mergeSort(inputSlice []int) []int {
+	if len(inputSlice) <= 1 {
+		return inputSlice
+	} else {
+		middleIndex := len(inputSlice) / 2
+		leftSlice := inputSlice[:middleIndex]
+		rightSlice := inputSlice[middleIndex:]
+		return merge(mergeSort(leftSlice), mergeSort(rightSlice))
+	}
+}
+
+func merge(slice1, slice2 []int) []int {
+	result := []int{}
+	for len(slice1) > 0 && len(slice2) > 0 {
+		if slice1[0] < slice2[0] {
+			result = append(result, slice1[0])
+			slice1 = slice1[1:]
+		} else {
+			result = append(result, slice2[0])
+			slice2 = slice2[1:]
+		}
+	}
+	if len(slice1) > 0 {
+		result = append(result, slice1...)
+	}
+	if len(slice2) > 0 {
+		result = append(result, slice2...)
+	}
+	return result
+}
---
+++ bubbleSort-Go/mergeSort_test.go
@@ -0,0 +1,43 @@
+package MySort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	t.Run("returns a sorted slice (2 items)", func(t *testing.T) {
+		original := []int{4, 2}
+		expected := []int{2, 4}
+
+		actual := mergeSort(original)
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected %v, but got %v", expected, actual)
+		}
+
+	})
+}
+
+func TestMerge(t *testing.T) {
+	t.Run("returns a combined slice from two single item slices", func(t *testing.T) {
+		slice1 := []int{2}
+		slice2 := []int{4}
+
+		expected := []int{2, 4}
+		got := merge(slice1, slice2)
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("expected %v, but got %v", expected, got)
+		}
+	})
+	t.Run("returns a combined slice", func(t *testing.T) {
+		slice1 := []int{2, 5, 9}
+		slice2 := []int{4, 7, 10, 100}
+
+		expected := []int{2, 4, 5, 7, 9, 10, 100}
+		got := merge(slice1, slice2)
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("expected %v, but got %v", expected, got)
+		}
+	})
+}
