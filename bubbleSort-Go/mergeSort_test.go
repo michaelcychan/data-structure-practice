d59b62f99cd8b49e99eab57da21661bf2d9c1c25
Michael Chan <michaelcychan@gmail.com>
2023-01-06 15:43:07 +0000

    add a more comprehensive test for merge sort

--- bubbleSort-Go/mergeSort_test.go
+++ bubbleSort-Go/mergeSort_test.go
@@ -15,7 +15,16 @@ func TestMergeSort(t *testing.T) {
 		if !reflect.DeepEqual(expected, actual) {
 			t.Errorf("expected %v, but got %v", expected, actual)
 		}
+	})
+	t.Run("returns a sorted slice (10 items)", func(t *testing.T) {
+		original := []int{6, 4, 2, 20, 16, 14, 18, 10, 12, 8}
+		expected := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
 
+		actual := mergeSort(original)
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected %v, but got %v", expected, actual)
+		}
 	})
 }
 
