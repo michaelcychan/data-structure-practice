b6ac62281a41f1ec6c8f33d58d4629ead4e7b787
Michael Chan <michaelcychan@gmail.com>
2023-01-20 19:52:08 +0000

    trees - add tests

--- trees/treenode/TreeNode_test.go
+++ trees/treenode/TreeNode_test.go
@@ -3,10 +3,30 @@ package TreeNode
 import "testing"
 
 func TestTreeNode(t *testing.T) {
-	newTreeNode := TreeNode{value: 100}
-	gotValue := newTreeNode.GetValue()
+	t.Run("create a TreeNode", func(t *testing.T) {
+		newTreeNode := TreeNode{value: 100}
+		gotValue := newTreeNode.GetValue()
 
-	if gotValue != 100 {
-		t.Errorf("expected the value to be %d, but got %d", 100, gotValue)
-	}
+		if gotValue != 100 {
+			t.Errorf("expected the value to be %d, but got %d", 100, gotValue)
+		}
+	})
+	t.Run("creating a TreeNode using CreateTreeNode()", func(t *testing.T) {
+		newTreeNode := CreateTreeNode(100)
+		gotValue := newTreeNode.GetValue()
+
+		if gotValue != 100 {
+			t.Errorf("expected the value to be %d, but got %d", 100, gotValue)
+		}
+	})
+	t.Run("change treenode value", func(t *testing.T) {
+		newTreeNode := CreateTreeNode(100)
+
+		newTreeNode.SetValue(105)
+		gotValue := newTreeNode.GetValue()
+		if gotValue != 105 {
+			t.Errorf("expected the value to be %d, but got %d", 100, gotValue)
+		}
+	})
+
 }
